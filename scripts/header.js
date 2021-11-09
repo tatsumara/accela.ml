b7545cd69ed89fcd13f3576104bbfc0b9c07badf
tatsumara <sutakumu@gmail.com>
2021-11-09 17:13:18 +0100

    Reordered links

--- scripts/header.js
+++ scripts/header.js
@@ -4,8 +4,8 @@ header.innerHTML = /*html*/ `
 	<div id="header-title">
 		<a href="./">Accela</a>
 	</div>
-	<a href="https://map.accela.ml/">Dynmap</a>
-	<a href="https://discord.gg/accela">Discord</a>
 	<a href="about.html">About</a>
+	<a href="https://discord.gg/accela">Discord</a>
+	<a href="https://map.accela.ml/">Dynmap</a>
 `
 document.body.appendChild(header)
