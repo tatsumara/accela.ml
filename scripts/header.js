dff972bbcdf4545561908732d4d51d7c86900d37
tatsumara <sutakumu@gmail.com>
2021-11-09 16:59:54 +0100

    Fixed formatting

--- scripts/header.js
+++ scripts/header.js
@@ -1,9 +1,11 @@
 const header = document.createElement('div');
 header.id = 'header';
-header.innerHTML = `<div id="header-title">
-					<a href="./">Accela</a>
-					</div>
-					<a href="https://map.accela.ml/">Dynmap</a>
-					<a href="https://discord.gg/accela">Discord</a>
-					<a href="about.html">About</a>`
+header.innerHTML = `
+	<div id="header-title">
+		<a href="./">Accela</a>
+	</div>
+	<a href="https://map.accela.ml/">Dynmap</a>
+	<a href="https://discord.gg/accela">Discord</a>
+	<a href="about.html">About</a>
+`
 document.body.appendChild(header)
